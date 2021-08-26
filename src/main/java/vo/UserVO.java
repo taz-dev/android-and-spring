65c6766537b31e1747cc0d0bcbb85484ca05d693
taz-dev <tazdev@naver.com>
2021-08-26 21:39:35 +0900

    android연동

---
+++ src/main/java/vo/UserVO.java
@@ -0,0 +1,35 @@
+package vo;
+
+public class UserVO {
+
+	private int idx;
+	private String id;
+	private String pwd;	
+	private String type;
+	
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+}
